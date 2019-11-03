4333a25b46b4d43512d9cced46269be3eb282544
Jonathan Bayless <baylessj@purdue.edu>
2019-11-03 18:35:29 -0500

    search icon moves properly

--- lib/sharedmodels/searchbar.dart
+++ lib/sharedmodels/searchbar.dart
@@ -13,13 +13,21 @@ class SearchBar extends StatefulWidget implements PreferredSizeWidget {
 class SearchBarState extends State<SearchBar> with TickerProviderStateMixin {
   AnimationController slideCtrl;
   var slideCurve;
+  bool inSearchMode = false;
+
+  animationStatusListener(AnimationStatus animationStatus) {
+    if (animationStatus == AnimationStatus.completed)
+      setState(() => inSearchMode = true);
+    else if (animationStatus == AnimationStatus.dismissed)
+      setState(() => inSearchMode = false);
+  }
 
   @override 
   void initState() {
-    slideCtrl = AnimationController(
-      vsync: this,
-      duration: Duration(milliseconds: 200)
-    )..addListener(() => setState(() {}));
+    slideCtrl = AnimationController(vsync: this, duration: Duration(milliseconds: 200))
+    ..addListener(() => setState(() {}))
+    ..addStatusListener(animationStatusListener);
+
     slideCurve = Tween(  
       begin: 0.0,
       end: 1.0,
@@ -39,49 +47,66 @@ class SearchBarState extends State<SearchBar> with TickerProviderStateMixin {
   @override 
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
+    Widget title = (inSearchMode) ? Container() : Text(
+      'auToDo',
+      style: logoStyle,
+    );
+      print(inSearchMode);
+
     return Container(
       width: width, 
       height: widget.preferredSize.height,
       color: Colors.transparent,
       child: SafeArea(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Row(  
-              children: <Widget>[
-                IconButton(  
-                  icon: Icon(Icons.menu),
-                  onPressed: () => Scaffold.of(context).openDrawer(),
-                ),
-                // Container is used to pad the left side and force 
-                // the title to the center
-                Container(
-                  width: 48 // normal Material Tap Target Size is 48px
-                ),
-              ],
-            ),
-            Text(
-              'auToDo',
-              style: logoStyle,
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                IconButton(  
-                  icon: Icon(Icons.search),
-                  onPressed: () {},
-                ),
-                IconButton(  
-                  icon: Icon(Icons.filter_list),
-                  onPressed: () => showDialog(
-                    context: context,
-                    builder: (context) => CarFilters(() => setState(() {})),
+        child: Container(
+          constraints: BoxConstraints(
+            maxWidth: width,
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(  
+                children: <Widget>[
+                  IconButton(  
+                    icon: Icon(Icons.menu),
+                    onPressed: () => Scaffold.of(context).openDrawer(),
                   ),
-                ),
-              ],
-            )
-          ],
+                  // Container is used to pad the left side and force 
+                  // the title to the center
+                  Container(
+                    width: (inSearchMode) ? 0 : 48 // normal Material Tap Target Size is 48px
+                  ),
+                ],
+              ),
+              title,
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Container( 
+                    width: (slideCurve.value * (width / 2 + 36)) + 48, // currently a magic number for alignment, figure this out
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: IconButton(  
+                        icon: Icon(Icons.search),
+                        onPressed: () {
+                          if (slideCtrl.isAnimating) return;
+                          (inSearchMode) ? slideCtrl.reverse() : slideCtrl.forward();
+                        }
+                      ),
+                    ),
+                  ),
+                  IconButton(  
+                    icon: Icon(Icons.filter_list),
+                    onPressed: () => showDialog(
+                      context: context,
+                      builder: (context) => CarFilters(() => setState(() {})),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
